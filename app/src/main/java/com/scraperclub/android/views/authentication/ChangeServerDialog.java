77288c553833eba01acec1821c1f4c3585047f74
user <email@user.com>
2019-02-21 14:21:00 +0200

    Update ChangeServerDialog class. Fix NPE in constructor. Add listener interface to notify caller that server was changed. Make EditText serverUrl as class field, add it to release method.

--- app/src/main/java/com/scraperclub/android/views/authentication/ChangeServerDialog.java
+++ app/src/main/java/com/scraperclub/android/views/authentication/ChangeServerDialog.java
@@ -2,6 +2,7 @@ package com.scraperclub.android.views.authentication;
 
 import android.content.Context;
 import android.support.v7.app.AlertDialog;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,18 +15,23 @@ import java.net.URL;
 
 public class ChangeServerDialog{
 
+    public interface Listener {
+        void onServerUpdate(String newServerUrl);
+    }
+
     private AlertDialog dialog;
+    private EditText serverUrl;
 
-
-    public ChangeServerDialog(Context context) {
+    public ChangeServerDialog(Context context, Listener listener) {
+        View dialogView = View.inflate(context,R.layout.fragment_change_server,null);
         dialog = new AlertDialog.Builder(context)
-                .setView(R.layout.fragment_change_server)
+                .setView(dialogView)
                 .create();
 
-        EditText serverUrl = dialog.findViewById(R.id.change_server_url_input);
-        Button clearButton = dialog.findViewById(R.id.change_server_clear);
-        Button cancelButton = dialog.findViewById(R.id.change_server_cancel);
-        Button saveButton = dialog.findViewById(R.id.change_server_save);
+        serverUrl = dialogView.findViewById(R.id.change_server_url_input);
+        Button clearButton = dialogView.findViewById(R.id.change_server_clear);
+        Button cancelButton = dialogView.findViewById(R.id.change_server_cancel);
+        Button saveButton = dialogView.findViewById(R.id.change_server_save);
 
         clearButton.setOnClickListener(v -> serverUrl.setText(""));
         cancelButton.setOnClickListener(v -> dismiss());
@@ -35,6 +41,7 @@ public class ChangeServerDialog{
             try {
                 URL newServerUrl = new URL(serverUrlText);
                 ScraperApp.getInstance().setServerUrl(newServerUrl);
+                listener.onServerUpdate(serverUrlText);
                 dismiss();
             } catch (MalformedURLException e) {
                 Toast.makeText(context, "Please enter correct URL", Toast.LENGTH_SHORT).show();
@@ -44,7 +51,6 @@ public class ChangeServerDialog{
 
     public void show(){
         if(dialog!=null && !dialog.isShowing()){
-            EditText serverUrl = dialog.findViewById(R.id.change_server_url_input);
             serverUrl.setText(ScraperApp.getInstance().getServerUrl());
             dialog.show();
         }
@@ -58,5 +64,6 @@ public class ChangeServerDialog{
 
     public void release(){
         dialog = null;
+        serverUrl = null;
     }
 }
