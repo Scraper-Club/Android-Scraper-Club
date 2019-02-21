0c7d9c5c9e821be09171df8f01d1f36d46ee4f66
user <email@user.com>
2019-02-21 14:21:57 +0200

    Implement ChangeServerDialog logic.

--- app/src/main/java/com/scraperclub/android/views/authentication/LoginActivityBase.java
+++ app/src/main/java/com/scraperclub/android/views/authentication/LoginActivityBase.java
@@ -5,20 +5,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.scraperclub.android.R;
+import com.scraperclub.android.ScraperApp;
 import com.scraperclub.android.api.model.ApiKey;
 import com.scraperclub.android.ScraperCore;
 import com.scraperclub.android.presenter.LoginControllerBase;
 
 public abstract class LoginActivityBase<T> extends AppCompatActivity implements LoginView {
 
+    protected ChangeServerDialog dialog;
     protected TextView changeServerLink;
     protected TextView serverUrlTextView;
+
     protected ProgressDialog progressDialog = null;
     protected String progressMessage;
 
@@ -37,6 +44,15 @@ public abstract class LoginActivityBase<T> extends AppCompatActivity implements 
                     controller.login(getInput());
                 }
         );
+        serverUrlTextView.setText(ScraperApp.getInstance().getServerUrl());
+        changeServerLink.setOnClickListener(v -> showChangeServerDialog());
+    }
+
+    private void showChangeServerDialog(){
+        if(dialog == null){
+            dialog = new ChangeServerDialog(this, serverUrlTextView::setText);
+        }
+        dialog.show();
     }
 
     protected abstract T getInput();
