7dd8aa55ff462f0ad01fd832607292d989fb568a
user <email@user.com>
2019-02-21 10:56:03 +0200

    Add ServerUrl loading and saving logic to ScraperApp class.

--- app/src/main/java/com/scraperclub/android/ScraperApp.java
+++ app/src/main/java/com/scraperclub/android/ScraperApp.java
@@ -1,9 +1,12 @@
 package com.scraperclub.android;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 
 import com.scraperclub.android.api.APIUrlsResolver;
+import com.scraperclub.android.api.BuildConfig;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ScraperApp extends Application {
@@ -24,6 +27,31 @@ public class ScraperApp extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        APIUrlsResolver.setServerURL(
+                loadUrlFromPreferences()
+        );
     }
-    
+
+    private URL loadUrlFromPreferences(){
+        SharedPreferences preferences = getSharedPreferences(ScraperApp.PREFERENCES_NAME,MODE_PRIVATE);
+        String serverUrlString = preferences.getString(PREFERENCES_SERVER_URL, BuildConfig.SERVER_URL);
+        URL serverUrl;
+        try {
+            serverUrl = new URL(serverUrlString);
+        } catch (MalformedURLException e) {
+            try {
+                serverUrl = new URL(BuildConfig.SERVER_URL);
+            } catch (MalformedURLException e1) {
+                throw new RuntimeException("Bad built-in Server URL");
+            }
+        }
+
+        return serverUrl;
+    }
+
+    public void updateServerURL(URL newServerUrl){
+        APIUrlsResolver.setServerURL(newServerUrl);
+        SharedPreferences preferences = getSharedPreferences(ScraperApp.PREFERENCES_NAME,MODE_PRIVATE);
+        preferences.edit().putString(PREFERENCES_SERVER_URL,newServerUrl.toString()).apply();
+    }
 }
